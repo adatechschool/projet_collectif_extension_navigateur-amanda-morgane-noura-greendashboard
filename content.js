da1c703bac35314c51b93b4296755485eef0ad79
morganelepine <114992081+morganelepine@users.noreply.github.com>
2023-01-17 10:15:31 +0100

    Ajout CSS

--- content.js
+++ content.js
@@ -4,4 +4,5 @@ let badge = document.createElement("p");
 badge.textContent = "Et on n'oublie pas sa pensée écolo du jour 🌿☘️ ! ";
 badge.style.fontSize = "17px";
 badge.style.fontStyle= "italic";
+badge.style.color = "#86d72f";
 (article ?? heading).insertAdjacentElement("beforeend", badge);
